891b3e235f9a7861b60eb9a30ba71c9a8ca7ff8d
Vinny <>
2023-06-08 13:46:51 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,105 @@
+const totalMechs = 909;
+const base_url = 'https://m.cyberbrokers.com/eth/mech/';
+
+let randomIndex1, randomIndex2;
+let imageScores = {};
+
+window.onload = async function() {
+    await loadScores();
+    displayRandomImages();
+    displaySortedImages();
+};
+
+function displayRandomImages() {
+    const image1 = document.getElementById('image1');
+    const image2 = document.getElementById('image2');
+    
+    // Get two distinct random indices
+    do {
+        randomIndex1 = Math.floor(Math.random() * totalMechs);
+        randomIndex2 = Math.floor(Math.random() * totalMechs);
+    } while (randomIndex1 === randomIndex2 || imageList[randomIndex1] == '' || imageList[randomIndex2] == '');
+    
+    // Set the source of the images to the URLs at the random indices
+    image1.src = imageList[randomIndex1];
+    image2.src = imageList[randomIndex2];
+}
+
+async function increaseScoreLeft() {
+    await updateAndLoadScores(randomIndex1);
+    displayRandomImages();
+    displaySortedImages();
+}
+
+async function increaseScoreRight() {
+    await updateAndLoadScores(randomIndex2);
+    displayRandomImages();
+    displaySortedImages();
+}
+
+function displaySortedImages(){
+    // Sort image data by score in descending order
+    let orderedImages = getSortedKeys(imageScores);
+
+    // Display the sorted images in the side panel
+    const imageSortedList = document.getElementById('image-list');
+    imageSortedList.innerHTML = '';
+    orderedImages.forEach(index => {
+        let score = imageScores[index];
+        let imageItem = document.createElement('div');
+        imageItem.className = 'image-item';
+        imageItem.innerHTML = `
+            <img src="${imageList[index]}" alt="Image" width="100">
+            <span class="image-score">${score}</span>
+        `;
+        imageSortedList.appendChild(imageItem);
+    });
+}
+
+function getSortedKeys(obj) {
+    var keys = Object.keys(obj);
+    return keys.sort(function(a,b){return obj[b]-obj[a]});
+}
+
+async function updateScore(tokenId){
+    let url = 'https://mech-models.glitch.me/mech-score?id='+tokenId;
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    return jsonData;
+}
+
+async function getScores(){
+    let url = 'https://mech-models.glitch.me/mech-scores';
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    return jsonData;
+}
+
+async function updateAndLoadScores(tokenId){
+    await updateScore(tokenId);
+    await loadScores();
+}
+
+async function loadScores(){
+    let scores = await getScores();
+    imageScores = {};
+    scores.forEach((score)=>{
+        imageScores[score.token_id] = score.score;
+    })
+}
+
+let imageList2 = [];
+async function getImage(tokenId){
+    let url = base_url + tokenId;
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    return jsonData.image;
+}
+
+async function getAllImages(){
+    for(let i=838; i<totalMechs; i++){
+        let url = await getImage(i);
+        console.log(i, url);
+        imageList2.push(url);
+    }
+}
